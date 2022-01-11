4bc3ce4beca1eb96b0c0c94e1a3bdfe29c36ff67
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-01-11 14:29:03 +0100

    ajout h1 en rose
    changement des h1 en couleur rose

--- tutoriels/laureuh/background.js
+++ tutoriels/laureuh/background.js
@@ -1,8 +1,17 @@
 // background.js
 
-let color = '#3aa757';
+// changement couleur background
+// let color = '#3aa757';
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
+
+// changement couleur h1
+let textColor = "#f999b7";
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  chrome.storage.sync.set({ textColor });
+  console.log('Default text color set to %cpink', `color: ${textColor}`);
 });
--- tutoriels/laureuh/popup.js
+++ tutoriels/laureuh/popup.js
@@ -1,23 +1,51 @@
-let changeColor = document.getElementById("changeColor");
+// changement background color en kesketuveu
+// let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// // When the button is clicked, inject setPageBackgroundColor into current page
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       function: setPageBackgroundColor,
+//     });
+//   });
+  
+//   // The body of this function will be executed as a content script inside the
+//   // current page
+//   function setPageBackgroundColor() {
+//     chrome.storage.sync.get("color", ({ color }) => {
+//       document.body.style.backgroundColor = color;
+//     });
+//   }
+
+
+  // changement h1 en couleur rose
+  let changeTextColor = document.getElementById("changeTextColor");
+
+  //changement du carrrrééééé dans la poppop
+  chrome.storage.sync.get("textColor", ({ textColor }) => {
+    changeTextColor.style.backgroundColor = textColor;
   });
   
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
+  // When the button is clicked, inject setPageBackgroundColor into current page
+  changeTextColor.addEventListener("click", async () => {
+      let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageTextColor,
+      });
     });
-  }
+    
+    // The body of this function will be executed as a content script inside the
+    // current page
+    function setPageTextColor() {
+      chrome.storage.sync.get("textColor", ({ textColor }) => {
+        document.querySelector("div").style.color = textColor;
+      });
+    }
