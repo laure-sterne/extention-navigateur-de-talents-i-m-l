634f119b59456d96703e312e33cd552fe2c66394
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-01-11 14:38:23 +0100

    retirer les commentaires inutiles

--- tutoriels/laureuh/background.js
+++ tutoriels/laureuh/background.js
@@ -1,12 +1,12 @@
 // background.js
 
 // changement couleur background
-// let color = '#3aa757';
+let color = '#3aa757';
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-//   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
 
 // changement couleur h1
 let textColor = "#f999b7";
