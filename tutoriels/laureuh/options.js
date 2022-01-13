5690fd63d35700ae7ee9be391f96caaf8bf38e88
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-01-13 09:52:59 +0100

    changement des images par des GIFs

--- tutoriels/laureuh/options.js
+++ tutoriels/laureuh/options.js
@@ -1,5 +1,4 @@
 //choix du background
-
 let page = document.getElementById("buttonDiv");
 let selectedClassName = "current";
 const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
@@ -50,7 +49,6 @@ constructOptions(presetButtonColors);
 
 
 //choix du GIF
-
 let gif = document.getElementById("buttonGif");
 let chosenClassName = "current";
 const presetButtonGifs = ["https://media.giphy.com/media/LxSFsOTa3ytEY/giphy.gif", "https://media.giphy.com/media/Kk9S4JvX439V6/giphy.gif", "https://media.giphy.com/media/Ev2Ov4wBz9Ogg/giphy.gif", "https://media3.giphy.com/media/3CWaVWrNJv27P4sPHR/giphy.gif?cid=6104955e422d685ec63161b7b494506866f4e14433187fde&rid=giphy.gif&ct=s"];
@@ -72,11 +70,14 @@ function handleButtonGifClick(event) {
 //fonction qui va créer nos boutons pour choisir le GIF
 function constructGifOptions(buttonGifs) {
   chrome.storage.sync.get("pokemonGif", (data) => {
-    let currentGif = data.color;
+    let currentGif = data.color
     for (let buttonGif of buttonGifs) {
       let button = document.createElement("button");
-      button.dataset.color = buttonGif;
-      button.style.backgroundImage = buttonGif;
+      // button.dataset.pokemonGif = buttonGif;
+      console.log( "Coucou", button, );
+      let url = "background-image: url("+pokemonGif+")";
+      button.style = url;
+      console.log( "Uesh", button, );
 
       if (buttonGif === currentGif) {
         button.classList.add(chosenClassName);
