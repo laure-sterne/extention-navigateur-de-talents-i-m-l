8b117d22c6782414226f8eda36d36a47ab13cadd
Isciane <isciane-nicolle@hotmail.fr>
2022-01-17 14:17:14 +0100

    Data colors json
    get the color's brightness of the background

--- daltonismeProject/background.js
+++ daltonismeProject/background.js
@@ -1,18 +1,30 @@
 // Première option : contraste des couleurs
 
-let contrastColor = "constrast(1.75)"
+// let contrastColor = "constrast(1.75)"
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ contrastColor });
+//   console.log('Default color set to %cconstrat', `color: ${contrastColor}`);
+// });
+
+
+// Deuxième option : mettre en évidence les liens si le background est foncé
+
+
+let highlightLinkDark = "#FF5733"
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ contrastColor });
-  console.log('Default color set to %cconstrat', `color: ${contrastColor}`);
+  chrome.storage.sync.set({ highlightLinkDark });
+  console.log('Default text link set to %csaumon', `color: ${highlightLinkDark}`);
+});
+
+// Troisième option : mise en évidence les liens si le background est clair :
+
+let highlightLinkLight = "#f9e939"
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ highlightLinkLight });
+  console.log('Default text link set to %cyellow', `color: ${highlightLinkLight}`);
 });
 
 
-// Deuxième option : mettre en évidence les liens
-
-let highlightLink = "#FF5733"
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ highlightLink });
-  console.log('Default text link set to %csaumon', `color: ${highlightLink}`);
-});
--- daltonismeProject/popup.js
+++ daltonismeProject/popup.js
@@ -1,33 +1,36 @@
 // Première option : augmenter les contrastes 
 
 
-let contrast = document.getElementById("contrast");
+// let contrast = document.getElementById("contrast");
 
-chrome.storage.sync.get("contrastColor", ({contrastColor}) => {
-  contrast.style.backgroundColor = contrastColor
-});
+// chrome.storage.sync.get("contrastColor", ({contrastColor}) => {
+//   contrast.style.backgroundColor = contrastColor
+// });
 
-contrast.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active : true, currentWindow: true});
+// contrast.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active : true, currentWindow: true});
 
-  chrome.scripting.executeScript({
-    target : { tabId: tab.id },
-    function : setColorContrast,
-  });
-});
+//   chrome.scripting.executeScript({
+//     target : { tabId: tab.id },
+//     function : setColorContrast,
+//   });
+// });
 
 
-function setColorContrast() {
-  chrome.storage.sync.get("contrastColor", ({contrastColor}) => {
-    let 
-  })
-}
+// function setColorContrast() {
+//   chrome.storage.sync.get("contrastColor", ({contrastColor}) => {
+//     let 
+//   })
+// }
 
-// Deuxième option : mettre en évidence les liens
+
+var requestURL = ''; 
+
+// Deuxième option : mettre en évidence les liens si le background est foncé 
 
 let highlight = document.getElementById("highlight");
 
-chrome.storage.sync.get("highlightLink", ({ highlightLink }) => {
+chrome.storage.sync.get("highlightLinkLight", ({ highlightLinkClear }) => {
   highlight.style.backgroundColor = highlightLink
 });
 
@@ -40,18 +43,19 @@ highlight.addEventListener("click", async () => {
   });
 });
 
+let colorBrightness = element.style.getPropertyValue("--backgroundColor");
 function setPageLink() {
-  chrome.storage.sync.get("highlightLink", ({ highlightLink }) => {
-    let listeLien = document.querySelectorAll("a")
-
+  if(colorBrightness <= 50%)
+    chrome.storage.sync.get("highlightLinkLight", ({ highlightLinkLight }) => {
+      let listeLien = document.querySelectorAll("a")
     for (i = 0; i < listeLien.length; i++) {
       console.log(listeLien[i])
-      listeLien[i].style.backgroundColor = highlightLink;
+      listeLien[i].style.backgroundColor = highlightLinkLight;
     }
   });
 }
 
-// Troisième option : visualiser les actions avec des icônes 
+
 
 
 
