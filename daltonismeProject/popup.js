dd3dcee77cb9b883301499cae6d5f1259894b824
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-01-18 16:09:41 +0100

    ajout de l'option page d'urgence
    mettre la page avec un fond noir et la police en blanc

--- daltonismeProject/popup.js
+++ daltonismeProject/popup.js
@@ -1,7 +1,4 @@
-
-
-
-// Deuxième option : mettre en évidence les liens si la checkboc est checked
+// Première option : mettre en évidence les liens si la checkboc est checked
 
 let highlight = document.getElementById("highlight");
 
@@ -25,7 +22,7 @@ function setPageLink() {
   
     for (i = 0; i < listeLien.length; i++) {
       if(listeLien[i].style.backgroundColor == highlightLink){
-        listeLien[i].style.backgroundColor = "transparent"
+        listeLien[i].style.backgroundColor = ''
       } else {
         listeLien[i].style.backgroundColor = highlightLink;
       }
@@ -34,7 +31,75 @@ function setPageLink() {
 }
 
 
-// Troisième option : visualiser les actions avec des icônes 
+// Deuxième option : URGENCE
+
+let blackAndWhite = document.getElementById("blackAndWhite");
+
+chrome.storage.sync.get("blackBackground", ({ blackBackground }) => {
+  blackAndWhite.style.backgroundColor = blackBackground
+});
+
+chrome.storage.sync.get("whiteFont", ({ whiteFont }) => {
+  blackAndWhite.style.color = whiteFont
+});
+
+blackAndWhite.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setSober,
+  });
+});
 
 
+function setSober() {
+  chrome.storage.sync.get("blackBackground", ({ blackBackground }) => {
+    if(document.body.style.backgroundColor !== blackBackground){
+      document.body.style.backgroundColor = blackBackground
+    } else {
+      document.body.style.backgroundColor = '';
+    }
+  });
 
+  chrome.storage.sync.get("whiteFont", ({ whiteFont }) => {
+    if(document.body.style.color !== whiteFont){
+      document.body.style.color = whiteFont
+    } else {
+      document.body.style.color = '';
+    }
+  });
+}
+
+
+// Troisième option : escroquerie
+
+// let highlight = document.getElementById("highlight");
+
+// chrome.storage.sync.get("highlightLink", ({ highlightLink }) => {
+//   highlight.style.backgroundColor = highlightLink
+// });
+
+// highlight.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: setPageLink,
+//   });
+// });
+
+
+// function setPageLink() {
+//   chrome.storage.sync.get("highlightLink", ({ highlightLink }) => {
+//     let listeLien = document.querySelectorAll("a")
+  
+//     for (i = 0; i < listeLien.length; i++) {
+//       if(listeLien[i].style.backgroundColor == highlightLink){
+//         listeLien[i].style.backgroundColor = ''
+//       } else {
+//         listeLien[i].style.backgroundColor = highlightLink;
+//       }
+//     }
+//   });
+// }
